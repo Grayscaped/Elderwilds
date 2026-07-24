8e8aa26170f679de1eaf9ac49d967ae84b2945d9
ByCubed7sLaptop <github@ThatOneProgrammer.uk>
2026-07-24 14:07:06 +0100

    Messing with header

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,27 @@
 import "./home.css";
 
 import Youtube from "../components/youtube.jsx";
+import Marquee from "../components/marquee.jsx";
 
 export default function page() {
   return (<>
+
+    {/* Backgrounds */}
     <div className="background background-image" />
 
     <div className="background" style={{top:"500px"}}>
         <svg viewBox="0 0 200 200">
-          <polygon points="100,0 200,10 200,200 0,200 0,10" fill="#785b8e" stroke="#000000" stroke-width="0" opacity="1" transform="rotate(0, 100, 100)" />
+          <polygon points="100,0 200,10 200,200 0,200 0,10" fill="#785b8e" stroke="#000000" strokeWidth="0" opacity="1" transform="rotate(0, 100, 100)" />
         </svg>
     </div>
 
+    <div className="background" style={{top:"1400px"}}>
+        <svg viewBox="0 0 200 200">
+          <polygon points="100,0 200,10 200,200 0,200 0,10" fill="#d0c8c1" stroke="#000000" strokeWidth="0" opacity="1" transform="rotate(0, 100, 100)" />
+        </svg>
+    </div>
+
+    {/* Sections */}
     <section>
         <h3>Elderwilds SMP</h3>
     </section>
@@ -19,19 +29,30 @@ export default function page() {
     <section className="center margin-bottom" style={{marginBottom: "200px"}}>
 
         <h1 className="title skranji-regular">Your adventure awaits</h1>
-        <div  className="subtitle margin-bottom">scrolling subheader build you legacy. write your own story</div>
-        <br/>
-        <div className="discord debug">Join!</div>
+        <Marquee  className="subtitle" speed="100" >Build you legacy. Write your own story. Create your legend. Define your future. Shape your destiny. Leave your mark. Own your journey. Forge your path. Chase greatness. Rise above. Stand apart.</Marquee>
+        <div className="margin-bottom"/>
+        <div className="discord impact" style={{fontSize:"1.5em"}}>Join!</div>
     </section>
 
-    <section className="center">
+    <section className="center margin-bottom">
         <Youtube src="https://www.youtube.com/embed/K0BWedP-jjY?autoplay=1&loop=1&controls=0&rel=0"></Youtube>
     </section>
 
     <section>
-        <div>image on left</div>
-        <div>title right</div>
-        <div>descrpition right</div>
+
+        <div className="card">
+        <div className="image">
+          <img src="https://via.placeholder.com/250" alt="Sample Image" />
+        </div>
+
+        <div className="content">
+          <h2>Title Here</h2>
+          <p>
+            This is the description. You can add any text here. The image appears
+            on the left while the title and description are displayed on the right.
+          </p>
+        </div>
+        </div>
 
 
           <main style={{ paddingTop: "80px" }}>
